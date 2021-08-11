3eea562133879c653cd1321bd40d2f9df7b3ae97
Timmesh <ktimmesh67@gmail.com>
2021-08-11 12:32:34 +0530

    Adding Auto-Logout

--- Angular/ReceipeRepo/src/app/auth/auth.service.ts
+++ Angular/ReceipeRepo/src/app/auth/auth.service.ts
@@ -17,6 +17,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: "root" })
 export class AuthService {
   user: BehaviorSubject<User> = new BehaviorSubject<User>(null);
+  private tokenExpirationTimer: any;
 
   constructor(private httpClient: HttpClient, private router:Router) {}
 
@@ -78,13 +79,26 @@ export class AuthService {
     }
     const loadedUser = new User(userData.email,userData.id, userData._token, new Date(userData._tokenExpirationDate));
     if(loadedUser.token) {
-      this.user.next(loadedUser)
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
   }
 
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() =>{
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleAuthentication(
@@ -96,6 +110,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, localId, idToken, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem("userData", JSON.stringify(user));
   }
 
