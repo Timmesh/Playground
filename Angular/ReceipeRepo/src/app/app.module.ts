20d56b899aee73cfb66f0ac32c3f057e062f464a
Timmesh <ktimmesh67@gmail.com>
2021-08-27 09:58:43 +0530

    Preloading Lazy Loaded code using providers in App Module

--- Angular/ReceipeRepo/src/app/app.module.ts
+++ Angular/ReceipeRepo/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core.module";
 import { HeaderComponent } from "./header/header.component";
+import { LoggingService } from "./logging.service";
 import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
@@ -22,6 +23,7 @@ import { SharedModule } from "./shared/shared.module";
     SharedModule,
     CoreModule
   ],
+  providers:[LoggingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
