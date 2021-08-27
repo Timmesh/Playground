1c20400975c6ecf43733826f985c5027bf20fff6
Timmesh <ktimmesh67@gmail.com>
2021-08-27 10:02:27 +0530

    Preloading Lazy Loaded code in Child component

--- Angular/ReceipeRepo/src/app/app.module.ts
+++ Angular/ReceipeRepo/src/app/app.module.ts
@@ -23,7 +23,6 @@ import { SharedModule } from "./shared/shared.module";
     SharedModule,
     CoreModule
   ],
-  providers:[LoggingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Angular/ReceipeRepo/src/app/shopping-list/shopping-list.module.ts
+++ Angular/ReceipeRepo/src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { LoggingService } from "../logging.service";
 import { SharedModule } from "../shared/shared.module";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListComponent } from "./shopping-list.component";
@@ -19,6 +20,7 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     SharedModule,
     FormsModule,
-  ]
+  ],
+  providers:[LoggingService]
 })
 export class ShoppingListModule {}
