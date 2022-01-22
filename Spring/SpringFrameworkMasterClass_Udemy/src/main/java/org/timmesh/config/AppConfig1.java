052790c9ec9e36e5938296d13b90b36465aeea42
Timmesh <ktimmesh67@gmail.com>
2022-01-22 23:47:42 +0530

    Lazy instantiation of bean

--- Spring/SpringFrameworkMasterClass_Udemy/src/main/java/org/timmesh/config/AppConfig1.java
+++ Spring/SpringFrameworkMasterClass_Udemy/src/main/java/org/timmesh/config/AppConfig1.java
@@ -2,6 +2,7 @@ package org.timmesh.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.timmesh.dao.DummyProductDao;
 import org.timmesh.dao.JdbcProductDao;
@@ -9,13 +10,14 @@ import org.timmesh.dao.JdbcProductDao;
 @Configuration
 public class AppConfig1 {
 
+	@Lazy
 	@Bean
 	public DummyProductDao dummyDao() {
 		System.out.println("AppConfig1.dummyDao() called");
 		return new DummyProductDao();
 	}
 
-	@Scope("prototype")
+	@Scope("singleton")
 	@Bean
 	public JdbcProductDao jdbcDao() {
 		System.out.println("AppConfig1.jdbcDao() called");
