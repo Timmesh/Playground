28f19b84cf06f9e76c48da50110d92db3f106078
Timmesh <ktimmesh67@gmail.com>
2022-01-23 00:39:34 +0530

    Reading from properties file

--- Spring/SpringFrameworkMasterClass_Udemy/src/main/java/org/timmesh/config/AppConfig1.java
+++ Spring/SpringFrameworkMasterClass_Udemy/src/main/java/org/timmesh/config/AppConfig1.java
@@ -1,15 +1,27 @@
 package org.timmesh.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.timmesh.dao.DummyProductDao;
 import org.timmesh.dao.JdbcProductDao;
 
 @Configuration
+@PropertySource("classpath:jdbc.properties")
 public class AppConfig1 {
 
+	@Value("${jdbc.driver}")
+	private String driverClassName;
+	@Value("${jdbc.url}")
+	private String url;
+	@Value("${jdbc.user}")
+	private String user;
+	@Value("${jdbc.password}")
+	private String password;
+	
 	@Lazy
 	@Bean
 	public DummyProductDao dummyDao() {
@@ -22,10 +34,10 @@ public class AppConfig1 {
 	public JdbcProductDao jdbcDao() {
 		System.out.println("AppConfig1.jdbcDao() called");
 		JdbcProductDao dao = new JdbcProductDao();
-		dao.setDriverClassName("org.h2.Driver");
-		dao.setUrl("jdbc:h2:tcp://localhost/~/spring-training");
-		dao.setUser("sa");
-		dao.setPassword("");
+		dao.setDriverClassName(driverClassName);
+		dao.setUrl(url);
+		dao.setUser(user);
+		dao.setPassword(password);
 		return dao;
 	}
 
