ac3696ae7dde4ce0c82643898da28a4c637ec933
Timmesh <ktimmesh67@gmail.com>
2022-01-23 03:14:57 +0530

    Adding toString using Lombok

--- Spring/SpringFrameworkMasterClass_Udemy/src/main/java/org/timmesh/entity/Category.java
+++ Spring/SpringFrameworkMasterClass_Udemy/src/main/java/org/timmesh/entity/Category.java
@@ -3,7 +3,9 @@ package org.timmesh.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString(exclude={"picture"})
 @NoArgsConstructor
 @Getter
 @Setter
