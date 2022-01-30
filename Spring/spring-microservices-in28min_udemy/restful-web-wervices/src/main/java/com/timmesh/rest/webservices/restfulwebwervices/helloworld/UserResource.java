15c774ed416eec7990b329937f2faeba09fc53d2
Timmesh <ktimmesh67@gmail.com>
2022-01-31 01:31:30 +0530

    Implementing POST

--- Spring/spring-microservices-in28min_udemy/restful-web-wervices/src/main/java/com/timmesh/rest/webservices/restfulwebwervices/helloworld/UserResource.java
+++ Spring/spring-microservices-in28min_udemy/restful-web-wervices/src/main/java/com/timmesh/rest/webservices/restfulwebwervices/helloworld/UserResource.java
@@ -3,8 +3,11 @@ package com.timmesh.rest.webservices.restfulwebwervices.helloworld;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +25,12 @@ public class UserResource {
 	public User retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
 		return user;
+	}
+	//
+	// input - details of user
+	// output - CREATED & Return the created URI
+	@PostMapping("/users")
+	public void createUser( @RequestBody User user) {
+		User savedUser = service.save(user);
 	}
 }
