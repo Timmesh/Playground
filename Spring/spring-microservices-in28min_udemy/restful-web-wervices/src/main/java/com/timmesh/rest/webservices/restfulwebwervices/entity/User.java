2a885f25f3aba55e07293752f0fa8806311c10a3
Timmesh <ktimmesh67@gmail.com>
2022-01-31 02:33:18 +0530

    Implementing Validations

--- Spring/spring-microservices-in28min_udemy/restful-web-wervices/src/main/java/com/timmesh/rest/webservices/restfulwebwervices/entity/User.java
+++ Spring/spring-microservices-in28min_udemy/restful-web-wervices/src/main/java/com/timmesh/rest/webservices/restfulwebwervices/entity/User.java
@@ -2,6 +2,9 @@ package com.timmesh.rest.webservices.restfulwebwervices.entity;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +20,10 @@ public class User {
 
 	private Integer id;
 
+	@Size(min=2, message="Name should have atleast 2 characters")
 	private String name;
 
+	@Past
 	private Date birthDate;
 
 }
--- Spring/spring-microservices-in28min_udemy/restful-web-wervices/src/main/java/com/timmesh/rest/webservices/restfulwebwervices/exception/CustomizedResponseEntityExceptionHandler.java
+++ Spring/spring-microservices-in28min_udemy/restful-web-wervices/src/main/java/com/timmesh/rest/webservices/restfulwebwervices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,16 +2,15 @@ package com.timmesh.rest.webservices.restfulwebwervices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.timmesh.rest.webservices.restfulwebwervices.helloworld.ExceptionResponse;
-import com.timmesh.rest.webservices.restfulwebwervices.helloworld.UserNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -31,4 +30,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Failed",
+				ex.getBindingResult().toString());
+		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
 }
