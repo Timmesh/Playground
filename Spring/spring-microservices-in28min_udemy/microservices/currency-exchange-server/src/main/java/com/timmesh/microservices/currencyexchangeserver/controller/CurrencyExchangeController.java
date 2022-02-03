ea1a0dff2568cf1859b843deb03429688c8bc734
Timmesh <ktimmesh67@gmail.com>
2022-02-04 02:42:00 +0530

    Adding Resource to Currency Exchange Microservice

---
+++ Spring/spring-microservices-in28min_udemy/microservices/currency-exchange-server/src/main/java/com/timmesh/microservices/currencyexchangeserver/controller/CurrencyExchangeController.java
@@ -0,0 +1,19 @@
+package com.timmesh.microservices.currencyexchangeserver.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.timmesh.microservices.currencyexchangeservice.CurrencyExchange;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(10));
+	}
+
+}
---
+++ Spring/spring-microservices-in28min_udemy/microservices/currency-exchange-server/src/main/java/com/timmesh/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,25 @@
+package com.timmesh.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CurrencyExchange {
+	private Long id;
+
+	private String from;
+
+	private String to;
+
+	private BigDecimal conversionMultiple;
+
+}
