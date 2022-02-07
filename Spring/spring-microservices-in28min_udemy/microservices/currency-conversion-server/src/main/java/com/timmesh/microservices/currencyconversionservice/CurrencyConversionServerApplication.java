d4f9480f5c2a31ed55273f6bfa69dede1315ebb7
Timmesh <ktimmesh67@gmail.com>
2022-02-07 17:37:20 +0530

    Added Currency Conversion SErver and Rest Template

---
+++ Spring/spring-microservices-in28min_udemy/microservices/currency-conversion-server/src/main/java/com/timmesh/microservices/currencyconversionservice/CurrencyConversionServerApplication.java
@@ -0,0 +1,13 @@
+package com.timmesh.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServerApplication.class, args);
+	}
+
+}
