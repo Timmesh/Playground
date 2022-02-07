178e567ee36900a052f111268df163889cf958d1
Timmesh <ktimmesh67@gmail.com>
2022-02-07 18:19:40 +0530

    Using Feign Client

--- Spring/spring-microservices-in28min_udemy/microservices/currency-conversion-server/src/main/java/com/timmesh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ Spring/spring-microservices-in28min_udemy/microservices/currency-conversion-server/src/main/java/com/timmesh/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.timmesh.microservices.currencyconversionservice.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.timmesh.microservices.currencyconversionservice.entity.CurrencyConversion;
+import com.timmesh.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	CurrencyExchangeProxy currencyExchangeProxy;
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
@@ -30,4 +35,19 @@ public class CurrencyConversionController {
 				quantity.multiply(currencyConversion.getConversionMultiple()), 
 				currencyConversion.getEnvironment()+ " " + "rest template");
 	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment() + " " + "feign");
+	}
+	
 }
---
+++ Spring/spring-microservices-in28min_udemy/microservices/currency-conversion-server/src/main/java/com/timmesh/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.timmesh.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.timmesh.microservices.currencyconversionservice.entity.CurrencyConversion;
+
+
+@FeignClient(name="currency-exchange", url="localhost:8001")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable(value = "from") String from, @PathVariable(value = "to") String to);
+
+}
