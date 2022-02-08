a1f47ed5084837751d1fb49a328892179779308f
Timmesh <ktimmesh67@gmail.com>
2022-02-08 17:42:25 +0530

    Adding Rate Limit

--- Spring/spring-microservices-in28min_udemy/microservices/currency-exchange-server/src/main/java/com/timmesh/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ Spring/spring-microservices-in28min_udemy/microservices/currency-exchange-server/src/main/java/com/timmesh/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
@@ -17,11 +18,13 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name="default")
 	public String sampleApi() {
 		logger.info("Sample api call received");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+		/*ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
 				String.class);
-		return forEntity.getBody();
+		return forEntity.getBody();*/
+		return "Sample-Api";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
