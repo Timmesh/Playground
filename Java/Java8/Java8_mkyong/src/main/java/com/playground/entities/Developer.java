0e2f69dc6e72b898f9d18e2ec0d790dcfd78f628
Timmesh <ktimmesh67@gmail.com>
2022-02-15 10:06:34 +0530

    Binary Operator

---
+++ Java/Java8/Java8_mkyong/src/main/java/com/playground/entities/Developer.java
@@ -0,0 +1,21 @@
+package com.playground.entities;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Developer {
+
+	String name;
+	BigDecimal salary;
+
+}
