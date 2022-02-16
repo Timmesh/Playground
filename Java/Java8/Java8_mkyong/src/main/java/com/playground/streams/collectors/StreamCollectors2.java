6fca2c8aaef3f764fd68be982bbf66cd5997bd79
Timmesh <ktimmesh67@gmail.com>
2022-02-16 12:25:19 +0530

    Collectors GroupingBy

---
+++ Java/Java8/Java8_mkyong/src/main/java/com/playground/streams/collectors/StreamCollectors2.java
@@ -0,0 +1,27 @@
+package com.playground.streams.collectors;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author USER
+ * Group by a List and display the total count of it and sort.
+ * 
+ */
+public class StreamCollectors2 {
+	public static void main(String[] args) {
+		// 3 apple, 2 banana, others 1
+		List<String> items = Arrays.asList("apple", "apple", "banana", "apple", "orange", "banana", "papaya");
+		Map<String, Long> result = items.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		Map<String, Long> finalMap = new LinkedHashMap<>();
+		// Sort a map and add to finalMap
+		result.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+				.forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+		System.out.println(finalMap);
+	}
+}
