f5aebc229981ff9a8face2dcbc383f541ff7bd64
Timmesh <ktimmesh67@gmail.com>
2022-02-24 08:03:08 +0530

    Printing Anagram together

--- DataStructuresAndAlgorithms/src/com/algorithms/programs/anagram/PrintAnagramTogetherMain.java
+++ DataStructuresAndAlgorithms/src/com/algorithms/programs/anagram/PrintAnagramTogetherMain.java
@@ -1,7 +1,11 @@
 package com.algorithms.programs.anagram;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author timmesh 
@@ -15,6 +19,11 @@ public class PrintAnagramTogetherMain {
 	}
 
 	private static Collection<List<String>> getAnagramsTogether(String[] arr) {
-		
+		Map<Map<String, Long>, List<String>> collect = Arrays.stream(arr)
+				.collect(Collectors.groupingBy(
+						(Function<String, Map<String, Long>>) s -> s.codePoints().mapToObj(Character::toString)
+								.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))));
+		return collect.values();
+
 	}
 }
