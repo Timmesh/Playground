fe3b056485bfd445ce5695d67e8b0efdcf9ba2f7
Timmesh <ktimmesh67@gmail.com>
2023-10-08 15:15:19 +0530

    Create Consumer

--- DevOps/kafka/MyConsumer/src/main/java/kafka/playground/MyConsumer.java
+++ DevOps/kafka/MyConsumer/src/main/java/kafka/playground/MyConsumer.java
@@ -1,7 +1,13 @@
 package kafka.playground;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class MyConsumer {
 
@@ -10,5 +16,15 @@ public class MyConsumer {
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "OrderGroup");
+
+        KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("my-topic"));
+        ConsumerRecords<String, Integer> records = consumer.poll(Duration.ofSeconds(60));
+        for (ConsumerRecord<String, Integer> record : records) {
+            System.out.println(record.key());
+            System.out.println(record.value());
+        }
+        consumer.close();
     }
 }
