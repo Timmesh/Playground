3ea5a0136588580c2e33ae63d94a7f9b12997d66
Timmesh <ktimmesh67@gmail.com>
2023-10-08 11:53:24 +0530

    Sync send

--- DevOps/kafka/KafkaProducer/src/main/java/kafka/playground/MyProducer.java
+++ DevOps/kafka/KafkaProducer/src/main/java/kafka/playground/MyProducer.java
@@ -3,8 +3,11 @@ package kafka.playground;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class MyProducer {
 
@@ -15,9 +18,12 @@ public class MyProducer {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
         try (KafkaProducer<String, Integer> kafkaProducer = new KafkaProducer<>(properties)) {
             ProducerRecord<String, Integer> record = new ProducerRecord<>("my-topic", "New Key", 300);
-            kafkaProducer.send(record);
+            RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+            System.out.println(recordMetadata.offset());
+            System.out.println(recordMetadata.partition());
             System.out.println("Completed");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 }
